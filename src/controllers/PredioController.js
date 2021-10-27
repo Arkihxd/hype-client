b8969223d567a271f1d6f4cbdd35c52b7755aa2f
Mateus Oliveira <mateusoliveiradesouza@hotmail.com>
2021-10-26 22:34:37 -0300

    Added Predio:controller functions

--- src/controllers/PredioController.js
+++ src/controllers/PredioController.js
@@ -2,25 +2,63 @@ const Predio = require('../models/Predio');
 
 module.exports = {
     async index(req, res){
-        
+        try{
+            const predios = await Predio.findAll();
+
+            return res.json(predios);
+        }catch(err){
+            res.status(500).json(err);
+        }
     },
 
     async show(req, res){
         const { predio_id } = req.params;
 
-        
+        try{
+            const predio = await Predio.findByPk(predio_id);
+
+            if(!predio){
+                return res.status(400).json({ error: 'Predio não existe' });
+            }
+
+            return res.json(predio);
+        }catch(err){
+            res.status(500).json(err);
+        }
     },
     
     async delete(req, res){
         const { predio_id } = req.params;
 
-        
+        try{
+            const predio = await Predio.findByPk(predio_id);
+
+            if(!predio){
+                return res.status(400).json({ error: 'Predio não existe' });
+            }
+
+            await Predio.destroy({
+                where: {id: predio_id}
+            });
+            return res.json();
+        }catch(err){
+            res.status(500).json(err);
+        }
     },
 
     async store(req, res){
         const { nome, sigla, endereco, cidade, estado } = 
         req.body;
 
-       
+        try{
+            const predio = await Predio.create({ nome, sigla, endereco, cidade, estado });
+
+            if(!predio){
+                return res.status(400).json({ error: 'Predio não existe' });
+            }
+            return res.json(predio);
+        }catch(err){
+            res.status(500).json(err);
+        }
     }
 };
