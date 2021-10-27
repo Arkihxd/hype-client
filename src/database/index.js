915e58ffb47caed6a19ec47eda77745a34a0f684
Mateus Oliveira <mateusoliveiradesouza@hotmail.com>
2021-10-26 22:26:28 -0300

    Added connection models in database

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,15 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
+const Predio = require('../models/Predio');
+const Apartamento = require('../models/Apartamento');
+
 const connection = new Sequelize(dbConfig);
+
+Predio.init(connection);
+Apartamento.init(connection);
+
+Predio.associate(connection.models);
+Apartamento.associate(connection.models);
 
 module.exports = connection;
