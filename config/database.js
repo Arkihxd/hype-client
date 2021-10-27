915f7c95346a4425f02a0c62cc339cead2b9562b
Mateus Oliveira <mateusoliveiradesouza@hotmail.com>
2021-10-26 21:21:28 -0300

    Configurated database config

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '123456',
+    logging: false,
+    database: 'desafio-hype',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
