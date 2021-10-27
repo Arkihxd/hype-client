3be96caff00efca89c09f4af231ceb3689025d92
Mateus Oliveira <mateusoliveiradesouza@hotmail.com>
2021-10-26 22:48:10 -0300

    Updated routes

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,18 @@
 const express = require('express');
+const PredioController = require('./controllers/PredioController');
+const ApartamentoController = require('./controllers/ApartamentoController');
+
 const routes = express.Router();
 
-routes.put('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
-routes.delete('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
-routes.get('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
-routes.get('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
+routes.put('/predios/create', PredioController.store);
+routes.delete('/predios/delete/:predio_id', PredioController.delete);
+routes.get('/predios', PredioController.index);
+routes.get('/predios/:predio_id', PredioController.show);
 
-
-routes.put('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
-routes.delete('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
-routes.get('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
-routes.get('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
-routes.get('/predios/teste', (req, res) => {
-    return res.json({response: "Teste de rota predios"});
-});
+routes.put('/apartamentos/create/:predio_id', ApartamentoController.store);
+routes.delete('/apartamentos/delete/:apartamento_id', ApartamentoController.delete);
+routes.get('/apartamentos', ApartamentoController.index);
+routes.get('/apartamentos/:apartamento_id', ApartamentoController.show);
+routes.get('/apartamentos/predios/:predio_id', ApartamentoController.showByPredios);
 
 module.exports = routes;
