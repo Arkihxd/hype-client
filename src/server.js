b2de780b32a71f8d73987a22247def3b9b5e2399
Mateus Oliveira <mateusoliveiradesouza@hotmail.com>
2021-10-29 19:07:22 -0300

    Testing heroku deploy

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const routes = require('./routes');
 const cors = require('cors');
 require('./database');
 
+const port = process.env.PORT || 3333;
+
 const app = express();
 app.use(cors())
 app.use(express.json()); 
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
 
